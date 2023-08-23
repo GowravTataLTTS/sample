0f41b0301808c5b02981b82c3e6b2b07c6ea65c5
GowravTataLTTS <113660597+GowravTataLTTS@users.noreply.github.com>
2023-08-23 09:44:43 +0000

    Added postgres extension

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your-secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///banking.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://username:password@db:5432/banking"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
